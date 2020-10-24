40d3374687464ebb0c9a9ef3e0c099371b9e96d3
Agris <7924@inbox.lv>
2020-10-25 01:52:38 +0300

    read code on data change confirmation modal

--- contentScript.js
+++ contentScript.js
@@ -1,6 +1,7 @@
 var cc = {};
 var username = "";
 var password = "";
+var isConfirmModalValueSet = false;
 
 window.onload = function () {
 
@@ -17,7 +18,7 @@ window.onload = function () {
             p.value = value.password || 'not-set';
         })
 
-        setTimeout(function (){
+        setTimeout(function () {
             document.querySelector('input[value="Sign me in"]').click();
 
             chrome.storage.local.get("codesInJson", value => {
@@ -31,8 +32,45 @@ window.onload = function () {
                 document.querySelector('#code_card_value').value = getCode(code);
                 document.querySelector('input[value="Authorise"]').click();
             }, 1000)
-        },100)
+        }, 100)
     }
+
+    setInterval(function () {
+
+        if(isConfirmModalValueSet){
+            return;
+        }
+
+        if(isEmpty(cc)){
+            chrome.storage.local.get("codesInJson", value => {
+                cc = value.codesInJson || {};
+            });
+            return;
+        }
+
+        let textObj = document.querySelector('#code_card>p>strong')
+
+        if(!textObj){
+            return;
+        }
+
+        let text = textObj.textContent
+        let code = parseInt(text);
+
+        let countOfCnfirmValuesSet = 0;
+
+        for (let i of document.querySelectorAll('input[name="code_card_code"]')) {
+            i.value = getCode(code);
+            if(i.value){
+                countOfCnfirmValuesSet++;
+            }
+        }
+
+        if(countOfCnfirmValuesSet > 0){
+            isConfirmModalValueSet = true;
+        }
+
+    }, 1000);
 }
 
 function getCode(key) {
@@ -40,3 +78,10 @@ function getCode(key) {
     return jsonCC[key];
 }
 
+function isEmpty(obj) {
+    for(var prop in obj) {
+        if(obj.hasOwnProperty(prop))
+            return false;
+    }
+    return true;
+}
